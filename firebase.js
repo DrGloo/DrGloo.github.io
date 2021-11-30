dfee3bae03d4bcd7b1e036933fb8513f99f845a8
Raishan Bernard <raishanbernard@gmail.com>
2021-11-30 13:53:36 -0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,19 @@
+var firebaseConfig = {
+apiKey: "AIzaSyDCAGVxtpauCQifCf_0Hh-85Nm8Cvlwv1o",
+
+  authDomain: "demon-ac9a0.firebaseapp.com",
+
+  projectId: "demon-ac9a0",
+
+  storageBucket: "demon-ac9a0.appspot.com",
+
+  messagingSenderId: "145522292433",
+
+  appId: "1:145522292433:web:bf89161acc4b9afcfab7f0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  var db = firebase.firestore();
+
